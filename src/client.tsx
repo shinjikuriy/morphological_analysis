cfc677283b3fcab518d41dc64271ca4599cdd527
shinjikuriy <35016959+shinjikuriy@users.noreply.github.com>
2025-06-11 20:37:28 +0900

    feat: enhance text input handling and improve UI structure in the analysis app

--- src/client.tsx
+++ src/client.tsx
@@ -1,13 +1,15 @@
 import { render } from 'preact'
-import { useState } from 'preact/hooks'
-import type { AnalysisResult, Token } from './types'
+import { useState, useId } from 'preact/hooks'
+import type { AnalysisResult } from './types'
 
 function App() {
   const [text, setText] = useState('')
   const [result, setResult] = useState<AnalysisResult | null>(null)
+  const formId = useId()
+  const inputId = useId()
 
   const handleChange = (e: Event) => {
-    setText((e.target as HTMLInputElement).value)
+    setText((e.target as HTMLTextAreaElement).value)
   }
 
   const handleSubmit = (e: Event) => {
@@ -33,40 +35,48 @@ function App() {
     <>
       <style>
         {`
+          .tokens span + span,
+          .content-words span + span {
+            margin-inline-start: 0.5em;
+          }
           .text-input {
             display: block;
             width: 30em;
             height: 10em;
             
           }
-          .content-words span + span {
-            margin-inline-start: 0.5em;
-          }
         `}
       </style>
       <h1>Morph Analysis</h1>
       <div>
-        input some text here:
-        <form onSubmit={handleSubmit}>
-          <textarea class='text-input' value={text} onChange={handleChange} />
+        <form id={formId} onSubmit={handleSubmit}>
+          <label for={inputId}>input some text here:</label>
+          <textarea
+            id={inputId}
+            class='text-input'
+            value={text}
+            onChange={handleChange}
+          />
           <button type='submit'>analyze</button>
         </form>
         {result && (
-          <div className='result'>
+          <div class='result'>
+            <h2>Tokens</h2>
+            <div class='tokens'>
+              {result.tokenList.map((token, index) => (
+                <span key={index}>{token.basic_form}</span>
+              ))}
+            </div>
             <h2>Content Words</h2>
-            <div className='content-words'>
+            <div class='content-words'>
               {result.contentWordList.map((word, index) => (
-                <span key={index}>
-                  {word.basic}
-                </span>
+                <span key={index}>{word.basic}</span>
               ))}
             </div>
             <h2>Kanji</h2>
-            <div className="kanjis">
+            <div class='kanjis'>
               {result.kanjiList.map((kanji, index) => (
-                <span key={index}>
-                  {kanji.kanji}
-                </span>
+                <span key={index}>{kanji.kanji}</span>
               ))}
             </div>
           </div>
